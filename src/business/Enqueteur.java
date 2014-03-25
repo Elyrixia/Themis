5c9dd2724c554ccd83a4aeb0662721432df9a87f
ig_2014 <ig_2014@igtp4-mac07.local>
2014-03-25 09:21:49 +0100

    Creation de DBFactory + Classes métiersDB

--- src/business/Enqueteur.java
+++ src/business/Enqueteur.java
@@ -1,21 +1,49 @@
 package business;
 
-public abstract class Enqueteur extends Personne {
+public abstract class Enqueteur extends Personne
+{
 
 	/**
 	 * The title of the detective (for example, "Lieutenant")
 	 */
 	private TitreEnqueteur titre;
-	
+
 	/**
 	 * The service where the detective works (for instance, police station)
 	 */
 	private ServiceEnqueteur service;
-	
+
 	/**
 	 * The personal phone number of the detective
 	 */
 	private String telephonePerso;
+
+<<<<<<< HEAD
+=======
+	public TitreEnqueteur getTitre() {
+		return titre;
+	}
+
+	public void setTitre(TitreEnqueteur titre) {
+		this.titre = titre;
+	}
+
+	public ServiceEnqueteur getService() {
+		return service;
+	}
+
+	public void setService(ServiceEnqueteur service) {
+		this.service = service;
+	}
+
+	public String getTelephonePerso() {
+		return telephonePerso;
+	}
+
+	public void setTelephonePerso(String telephonePerso) {
+		this.telephonePerso = telephonePerso;
+	}
 	
 	
+>>>>>>> ec95cd2582944916cb50aa43dca1d4b831c19b48
 }
--- src/persistence/Factory.java
+++ src/persistence/Factory.java
@@ -1,5 +1,12 @@
 package persistence;
 
-public abstract class Factory {
+import business.*;
+
+public abstract class Factory
+{
+	public abstract Enqueteur createEnqueteur();
+	public abstract CorpsEnqueteur createCorpsEnqueteur();
+	public abstract ServiceEnqueteur createServiceEnqueteur();
+	public abstract TitreEnqueteur createTitreEnqueteur();
 	
 }
