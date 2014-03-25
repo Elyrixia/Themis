f9d6c20884a9d2c5bc8191e7ab5ad61dd749b499
ig_2014 <ig_2014@igtp4-mac07.local>
2014-03-25 11:02:07 +0100

    lolxd

--- business/Titre.java
+++
@@ -1,9 +0,0 @@
-package business;
-
-public abstract class Titre {
-
-	/**
-	 * The name of the title (example: "Lieutenant")
-	 */
-	private String libelle;
-}
--- business/TitreEnqueteurDB.java
+++
@@ -1,6 +0,0 @@
-package business;
-
-public class TitreEnqueteurDB extends TitreEnqueteur
-{
-
-}
