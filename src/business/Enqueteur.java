ec95cd2582944916cb50aa43dca1d4b831c19b48
ruizb <benoit.ruiz01@gmail.com>
2014-03-25 09:05:00 +0100

    Suppression des fichiers bin/, .project et .classpath

--- src/business/Enqueteur.java
+++ src/business/Enqueteur.java
@@ -16,6 +16,30 @@ public abstract class Enqueteur extends Personne {
 	 * The personal phone number of the detective
 	 */
 	private String telephonePerso;
+
+	public TitreEnqueteur getTitre() {
+		return titre;
+	}
+
+	public void setTitre(TitreEnqueteur titre) {
+		this.titre = titre;
+	}
+
+	public ServiceEnqueteur getService() {
+		return service;
+	}
+
+	public void setService(ServiceEnqueteur service) {
+		this.service = service;
+	}
+
+	public String getTelephonePerso() {
+		return telephonePerso;
+	}
+
+	public void setTelephonePerso(String telephonePerso) {
+		this.telephonePerso = telephonePerso;
+	}
 	
 	
 }
