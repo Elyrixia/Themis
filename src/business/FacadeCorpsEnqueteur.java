d69fe76d777118ac0a74973050e67286df9eade9
Christophe-Pont <pont.christophe.pc@gmail.com>
2014-03-25 10:34:09 +0100

    Ajout de CorpsEnqueteurFacade

---
+++ src/business/FacadeCorpsEnqueteur.java
@@ -0,0 +1,75 @@
+package business;
+
+import java.util.ArrayList;
+
+public abstract class FacadeCorpsEnqueteur {
+
+	// ATTRIBUTES
+	
+	/**
+	 * Manager qui permet d'acceder aux entites souhaitees
+	 */
+	private CorpsEnqueteurManager ceMng;
+	
+	// METHODS
+	
+	/**
+	 * Permet d'ajouter un nouveau corps d'enqueteur
+	 * @libelle: Le libelle de ce corps
+	 * @titres: La liste des titres lies a ce corps
+	 */
+	public Exception ajouterCorpsEnqueteur(String libelle, ArrayList<TitreEnqueteur> titres) {
+		try {
+			CorpsEnqueteur newCorps = new CorpsEnqueteur();
+			newCorps.create(libelle,titres);
+			return 1;
+		} catch(Exception e) {
+			return 0;
+		}
+		
+	}
+	/**
+	 * Permet de modifier un corps d'enqueteur existant
+	 * @corps: L'entite a modifier
+	 * @libelle: Le nouveau libelle de ce corps
+	 */
+	public Exception modifierCorpsEnqueteur(CorpsEnqueteur corps, String libelle) {
+		try {
+			corps.setAttributes(libelle);
+			return 1;
+		} catch(Exception e) {
+			return 0;
+		}
+	}
+	
+	/**
+	 * Permet de supprimer un corps d'enqueteur existant
+	 * @corps: L'entite a supprimer
+	 */
+	public Exception supprimerCorpsEnqueteur(CorpsEnqueteur corps) {
+		try {
+			corps.delete();
+			return 1;
+		} catch(Exception e) {
+			return 0;
+		}
+	}
+	
+	/**
+	 * ?
+	 */
+	public void consulterCorpsEnqueteur(CorpsEnqueteur corps) {
+		
+	}
+	
+	/**
+	 * Permet de charger les corps d'enqueteur souhaites en fonction du filtre a appliquer
+	 * @filter: Le filtre a appliquer
+	 */
+	public ArrayList<CorpsEnqueteur> chargerCorpsEnqueteur(HashMap filter) {
+		ceMng = new CorpsEnqueteurManager();
+		ceMng.loadAffaires(filter);
+		return ceMng.listeAffaires;
+	}
+	
+}
