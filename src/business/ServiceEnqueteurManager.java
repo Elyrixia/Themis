3edb82dbecc59d2cab78662a567c5073ed406622
Elyrixia <salle.trist@gmail.com>
2014-03-27 17:03:54 +0100

    Updated Corps, Service, Titre, Enqueteur without new SQLManager

--- src/business/ServiceEnqueteurManager.java
+++ src/business/ServiceEnqueteurManager.java
@@ -1,7 +1,14 @@
 package business;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+
+import persistence.DBFactory;
+import persistence.Factory;
+import persistence.SQLManager;
 
 public class ServiceEnqueteurManager {
 
@@ -10,12 +17,12 @@ public class ServiceEnqueteurManager {
 	/**
 	 * Loaded list of ServiceEnqueteur
 	 */
-	private ArrayList<ServiceEnqueteur> listeServicesEnqueteur;
+	private ArrayList<ServiceEnqueteur> listeServices;
 		
 	// CONSTRUCTOR
 		
 	public ServiceEnqueteurManager() {
-		this.listeServicesEnqueteur = new ArrayList<ServiceEnqueteur>();
+		this.listeServices = new ArrayList<ServiceEnqueteur>();
 	}
 		
 	// METHODS
@@ -24,11 +31,58 @@ public class ServiceEnqueteurManager {
 	 * Load ServiceEnqueteur list using a filter
 	 * @param: filter: A DEFINIR
 	 */
-	public void loadServicesEnqueteur(HashMap<String,String> filter) {}
+	public void loadServicesEnqueteur(HashMap<String,String> filter) {
+		SQLManager connect = SQLManager.getConnection();
+		
+		String query = "SELECT * FROM service_enqueteur";
+		
+		// Si on a fourni un filtre il va falloir specifier le where
+		if(filter.size() > 0) {
+			query += " WHERE ";
+			
+			Iterator<String> keySetIterator = filter.keySet().iterator();
+			
+			// Premiere condition
+			String key = keySetIterator.next();
+			query += key + filter.get(key);
+			
+			// S'il y en a d'autres
+			while(keySetIterator.hasNext()) {
+				query += " AND ";
+				key = keySetIterator.next();
+				query += key + filter.get(key);
+			}
+		}
+		
+		// Sending query
+		ResultSet result = connect.querySelect(query);
+		
+		// Preparing ArrayList
+		try {
+			Factory fac = DBFactory.getInstance();
+			while(result.next()) {
+				// Create new ServiceEnqueteur
+				ServiceEnqueteur newService = fac.createServiceEnqueteur();
+				// Reading row in SQLResult
+				HashMap<String, Object> row = new HashMap<String, Object>();
+				row.put("id",result.getInt("id"));
+				row.put("libelle", result.getString("libelle"));
+				row.put("telephone", result.getString("telephone"));
+				row.put("lieu", result.getString("lieu"));
+				// Loading ServiceEnqueteur using values in row
+				newService.load(row);
+				// Adding ServiceEnqueteur to ArrayList
+				this.listeServices.add(newService);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 		
 	// GETTERS
 	
 	public ArrayList<ServiceEnqueteur> getListeServicesEnqueteur() {
-			return this.listeServicesEnqueteur;
-		}
+		return this.listeServices;
+	}
 }
--- src/ui/MainFenetre.java
+++ src/ui/MainFenetre.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JFrame;
@@ -7,6 +8,7 @@ import javax.swing.JFrame;
 import facade.FacadeCorpsEnqueteur;
 import business.CorpsEnqueteur;
 import business.CorpsEnqueteurDB;
+import business.ServiceEnqueteur;
 import business.Enqueteur;
 import persistence.*;
 
@@ -15,13 +17,23 @@ class MainFenetre extends JFrame
 	public static void main(String []args)
 	{
 		//Factory f = DBFactory.getInstance();
-		
 		FacadeCorpsEnqueteur fce = new FacadeCorpsEnqueteur();
 		
-		HashMap<String,String> filter = new HashMap<String,String>();
-		filter.put("id","=344");
-		//filter.put("libelle", "<>henri");
-		fce.chargerCorpsEnqueteur(filter);
+		try {
+			CorpsEnqueteur bob = fce.ajouterCorpsEnqueteur("boby", new ArrayList<ServiceEnqueteur>());
+			
+			HashMap<String,Object> map = fce.consulterCorpsEnqueteur(bob);
+			System.out.println(map.get(":libelle"));
+			
+			HashMap<String,String> filter = new HashMap<String,String>();
+			filter.put("id", "=3");
+			ArrayList<CorpsEnqueteur> test = fce.chargerCorpsEnqueteur(filter);
+			System.out.println(test.get(0).getLibelle());
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		/*Enqueteur e = f.createEnqueteur();
 		System.out.println(e.create());
