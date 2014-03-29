119fffd339c043513bb454a239b63039793cccfa
VegaWhite <marincommeunmarin@gmail.com>
2014-03-29 10:53:47 +0100

    Utilisation du System Look and Feel

--- src/ui/MainFenetre.java
+++ src/ui/MainFenetre.java
@@ -1,16 +1,29 @@
 package ui;
-public class MainFenetre {
-	
-	//Attributes :
-	private FenetreMenu window;
-	
-	//Constructors :
-	public MainFenetre(){
+
+import javax.swing.UIManager;
+
+public class MainFenetre
+{
+
+	// Attributes :
+	private FenetreMenu	window;
+
+	// Constructors :
+	public MainFenetre()
+	{
 		this.window = new EnqueteurFenetre();
 	}
 
-	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e)
+		{
+			System.err.println("Could not load the system Look and Feel");
+		}
 		MainFenetre mainWindow = new MainFenetre();
 	}
 }
